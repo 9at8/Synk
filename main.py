90fe04c6feabc7cfaa37ab0b3663aca309c1d825
9at8 <aditya.1998thakral@gmail.com>
2016-05-16 12:55:19 +0530

    v0.2.0 - Code actually works! xD

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ import shutil
 
 class Sync(object):
     def __init__(self):
-        def input_source():
-            src = raw_input('Enter location of source file/folder: ')
+        self.source = ['', '']
+        self.destination = ''
+        Sync.input_locations(self)
+
+    def copy(self):
+        if self.source[1] == 'file':
+            print 'Copying', self.source, 'to', self.destination, '...'
+            print 'Please be patient...\n'
+            shutil.copy2(self.source[0], self.destination)
+            print 'Mission Accomplished!'
+        elif self.source[1] == 'directory':
+            print 'Copying', self.source, 'to', self.destination, '...'
+            print 'Please be patient...\n'
+            try:
+                shutil.copytree(self.source[0], self.destination)
+            except OSError, e:
+                if e.errno == 17:
+                    print self.destination, 'already exists.'
+                    print self.destination, 'will be emptied. Press enter to continue.'
+                    x = raw_input()
+                    shutil.rmtree(self.destination)
+                    shutil.copytree(self.source[0], self.destination)
+            print 'Mission Accomplished!'
+
+    def input_locations(self):
+        def _input_source_():
+            src = raw_input('Enter location of source file/folder: ').rstrip('/')
+            print src
             source = [src, 'directory']
             try:
                 os.listdir(source[0])
@@ -13,47 +39,29 @@ class Sync(object):
             except OSError, e:
                 if e.errno == 2:
                     print 'File/Folder doesn\'t exist. Try again.\n'
-                    input_source()
+                    _input_source_()
                 elif e.errno == 20:
                     source[1] = 'file'
                     return source
 
-        def input_destination():
-            destination = raw_input('Enter location of \
-                                    destination folder: ')
-            try:
-                os.listdir(destination)
+        def _input_destination_():
+            destination = raw_input('Enter location of destination folder: ').rstrip('/')
+            if self.source[1] == 'directory':
                 return destination
-            except OSError, e:
-                if e.errno == 2 or e.errno == 20:
-                    choice = raw_input('Destination folder doesn\'t exist.\n\
-                                        Do you want to create a new folder \
-                                        of the same name? (y/n) ')
+            elif self.source[1] == 'file':
+                try:
+                    os.listdir(destination)
+                    return destination
+                except OSError, e:
+                    if e.errno == 2 or e.errno == 20:
+                        choice = raw_input('Destination folder doesn\'t exist.\n\
+Do you want to create a new folder of the same name? (y/n) ')
                     if choice.lower() == 'y':
                         os.mkdir(destination)
                         return destination
                     elif choice.lower() == 'n':
                         print 'Try again. \n'
-                        input_destination()
+                        _input_destination_()
 
-        self.source = input_source()
-        self.destination = input_destination()
-
-    def copy(self):
-        if self.source[1] == 'file':
-            shutil.copy2(self.source[0], self.destination)
-        elif self.source[1] == 'directory':
-            temp = self.source[0]
-            contents = os.listdir(self.source[0])
-            for file_name in contents:
-                print 'Copying', file_name, 'from', self.source
-                self.source[0] = temp
-                if self.source[0][-1] == '/':
-                    self.source[0] += file_name
-                elif self.source[0][-1] == '\\':
-                    self.source[0] += file_name
-                elif 'win' in os.uname()[0]:
-                    self.source[0] += '\\' + file_name
-                else:
-                    self.source[0] += '/' + file_name
-                Sync.copy(self)
+        self.source = _input_source_()
+        self.destination = _input_destination_()
